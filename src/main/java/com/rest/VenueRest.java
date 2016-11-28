602fa537b178b599618db34951e517f3ab8f9f94
Pablo Barrenechea <pbarrenechea@devspark.com>
2016-11-28 00:20:09 -0300

    Added rest API support

---
+++ src/main/java/com/rest/VenueRest.java
@@ -0,0 +1,53 @@
+package com.rest;
+
+/**
+ * Created by Usuario on 26/11/2016.
+ */
+public class VenueRest {
+    private long id;
+    private String name;
+    private String category;
+    private long latitude;
+    private long longitude;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public long getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(long latitude) {
+        this.latitude = latitude;
+    }
+
+    public long getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(long longitude) {
+        this.longitude = longitude;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
